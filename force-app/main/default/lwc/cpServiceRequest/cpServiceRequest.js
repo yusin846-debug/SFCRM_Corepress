6ca4647c2c2b984e1523a49ff3029e2f6d7f1126
yusin846-debug <yusin846@gmail.com>
2026-08-18 19:04:10 +0900

    Refine service request upload and submit controls

--- force-app/main/default/lwc/cpServiceRequest/cpServiceRequest.js
+++ force-app/main/default/lwc/cpServiceRequest/cpServiceRequest.js
@@ -6,6 +6,16 @@ export default class CpServiceRequest extends LightningElement {
     @api assetListUrl = '/asset-list';
     headerLogoUrl = headerLogo;
     isSubmitted = false;
+    selectedFileLabel = '선택된 파일 없음';
+
+    handleFileChange(event) {
+        const files = [...event.target.files];
+        this.selectedFileLabel = files.length === 0
+            ? '선택된 파일 없음'
+            : files.length === 1
+                ? files[0].name
+                : `${files[0].name} 외 ${files.length - 1}개`;
+    }
 
     handleSubmit(event) {
         event.preventDefault();
@@ -26,5 +36,6 @@ export default class CpServiceRequest extends LightningElement {
 
     resetForm() {
         this.isSubmitted = false;
+        this.selectedFileLabel = '선택된 파일 없음';
     }
 }
