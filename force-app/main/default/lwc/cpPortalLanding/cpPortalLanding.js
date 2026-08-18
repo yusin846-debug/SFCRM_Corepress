665d590fb637903e13ee85812fc90bc888e35970
yusin846-debug <yusin846@gmail.com>
2026-08-18 13:06:39 +0900

    fix: replace hero infographic icons with PNG assets

--- force-app/main/default/lwc/cpPortalLanding/cpPortalLanding.js
+++ force-app/main/default/lwc/cpPortalLanding/cpPortalLanding.js
@@ -4,6 +4,9 @@ import headerLogo from '@salesforce/resourceUrl/CorePressHeaderLogo';
 import cp100Image from '@salesforce/resourceUrl/CorePressCP100';
 import cp2100Image from '@salesforce/resourceUrl/CorePressCP2100';
 import cp7100Image from '@salesforce/resourceUrl/CorePressCP7100';
+import healthIcon from '@salesforce/resourceUrl/CorePressHealthIcon';
+import supportIcon from '@salesforce/resourceUrl/CorePressSupportIcon';
+import maintenanceIcon from '@salesforce/resourceUrl/CorePressMaintenanceIcon';
 
 export default class CpPortalLanding extends LightningElement {
     @api heroTitle = '공정을 멈추지 않는 서비스';
@@ -16,4 +19,7 @@ export default class CpPortalLanding extends LightningElement {
     cp100ImageUrl = cp100Image;
     cp2100ImageUrl = cp2100Image;
     cp7100ImageUrl = cp7100Image;
+    healthIconUrl = healthIcon;
+    supportIconUrl = supportIcon;
+    maintenanceIconUrl = maintenanceIcon;
 }
