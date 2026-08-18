562f25729b86035a3f9a3318c3617464ba89cbce
yusin846-debug <yusin846@gmail.com>
2026-08-18 11:54:28 +0900

    refactor: match approved portal landing reference

--- force-app/main/default/lwc/cpPortalLanding/cpPortalLanding.js
+++ force-app/main/default/lwc/cpPortalLanding/cpPortalLanding.js
@@ -6,7 +6,7 @@ import headerLogo from '@salesforce/resourceUrl/CorePressHeaderLogo';
 export default class CpPortalLanding extends LightningElement {
     @api heroTitle = '공정을 멈추지 않는 서비스';
     @api heroDescription =
-        '설치부터 보증, 현장 서비스까지\n압축기 생애주기를 하나의 흐름으로 관리합니다.';
+        '설치부터 보증, 현장 서비스까지\n압축기의 전체 수명주기를 한곳에서 관리합니다.';
     @api loginUrl = 'login';
 
     coverImageUrl = coverImage;
