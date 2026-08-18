501188db71347d65fa898541a4519960eb9a5d03
yusin846-debug <yusin846@gmail.com>
2026-08-18 11:44:04 +0900

    design: align portal landing with approved reference

--- force-app/main/default/lwc/cpPortalLanding/cpPortalLanding.js
+++ force-app/main/default/lwc/cpPortalLanding/cpPortalLanding.js
@@ -5,7 +5,7 @@ import lineupImage from '@salesforce/resourceUrl/CorePressProductLineup';
 export default class CpPortalLanding extends LightningElement {
     @api heroTitle = '공정을 멈추지 않는 서비스';
     @api heroDescription =
-        '설비 정보부터 서비스 요청까지, CorePress 고객 포털에서 한 번에 연결합니다.';
+        '설치부터 보증, 현장 서비스까지\n압축기 생애주기를 하나의 흐름으로 관리합니다.';
     @api loginUrl = 'login';
 
     coverImageUrl = coverImage;
