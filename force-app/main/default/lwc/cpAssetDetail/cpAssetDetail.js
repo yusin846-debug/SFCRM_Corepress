6e92165b14cc6808dd72dde9d74669947b33202b
yusin846-debug <yusin846@gmail.com>
2026-08-18 17:10:32 +0900

    Build reference-matched asset detail page

--- force-app/main/default/lwc/cpAssetDetail/cpAssetDetail.js
+++ force-app/main/default/lwc/cpAssetDetail/cpAssetDetail.js
@@ -1,19 +1,25 @@
-import { LightningElement } from 'lwc';
-import logo from '@salesforce/resourceUrl/CorePressHeaderLogo';
-import cp7100 from '@salesforce/resourceUrl/CorePressCP7100';
-import locationIcon from '@salesforce/resourceUrl/CorePressLocationIcon';
-import operatingIcon from '@salesforce/resourceUrl/CorePressOperatingIcon';
-import logoutIcon from '@salesforce/resourceUrl/CorePressLogoutWhiteIcon';
+import { LightningElement, api } from "lwc";
+import logo from "@salesforce/resourceUrl/CorePressHeaderLogo";
+import cp7100 from "@salesforce/resourceUrl/CorePressCP7100Detail";
+import blueprint from "@salesforce/resourceUrl/CorePressCP7100Blueprint";
+import locationIcon from "@salesforce/resourceUrl/CorePressLocationIcon";
+import operatingIcon from "@salesforce/resourceUrl/CorePressSummaryOperatingIcon";
+import logoutIcon from "@salesforce/resourceUrl/CorePressLogoutWhiteIcon";
 
 export default class CpAssetDetail extends LightningElement {
-    logoUrl = logo;
-    cp7100Url = cp7100;
-    locationIconUrl = locationIcon;
-    operatingIconUrl = operatingIcon;
-    logoutIconUrl = logoutIcon;
+  @api homeUrl = "portal-home";
+  @api assetListUrl = "asset-list";
+  @api serviceListUrl = "service-requests";
+  @api serviceRequestUrl = "service-request-new";
+  logoUrl = logo;
+  cp7100Url = cp7100;
+  blueprintUrl = blueprint;
+  locationIconUrl = locationIcon;
+  operatingIconUrl = operatingIcon;
+  logoutIconUrl = logoutIcon;
 
-    handleLogout() {
-        const returnUrl = encodeURIComponent('/corepressvforcesite/s/');
-        window.location.assign(`/secur/logout.jsp?retUrl=${returnUrl}`);
-    }
+  handleLogout() {
+    const returnUrl = encodeURIComponent("/corepressvforcesite/s/");
+    window.location.assign(`/secur/logout.jsp?retUrl=${returnUrl}`);
+  }
 }
